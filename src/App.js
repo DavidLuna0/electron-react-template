97ae0a023d3451a1c871081375afa229ee7c8d0e
David <ddavidr569@gmail.com>
2020-01-24 17:36:21 -0300

    Refactoring code to have only the basic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import './index.css'
-import Homepage from './screens/homepage'
+import React from "react";
+import ReactLogo from "./react.logo.svg";
+import ElectronLogo from "./electron.logo.svg";
+import "./App.css";
 
-export default function App() {
+function App() {
   return (
-    <div class="container">
-        <Homepage />
+    <div className="App">
+      <header className="App-header">
+        <p className="header-text">Electron-React App Template</p>
+        <div class="logo-container">
+          <img src={ReactLogo} className="React-logo" alt="react" />
+          <img src={ElectronLogo} className="Electron-logo" alt="electron" />
+        </div>
+        <footer>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <p>Press Ctrl+Shift+I to toogle Developer Tools</p>
+        </footer>
+      </header>
     </div>
   );
 }
-    
+
+export default App;
